64e16dcc48feec2384cc1755ce62259a82594dbc
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-01-15 16:57:48 -0500

    Have a fall back in the even that MultipleHUD is not installed.

--- src/main/java/com/azuredoom/levelingcore/hud/XPTickSystem.java
+++ src/main/java/com/azuredoom/levelingcore/hud/XPTickSystem.java
@@ -1,6 +1,8 @@
 package com.azuredoom.levelingcore.hud;
 
+import com.azuredoom.levelingcore.LevelingCore;
 import com.buuz135.mhud.MultipleHUD;
+import com.hypixel.hytale.common.plugin.PluginIdentifier;
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Holder;
@@ -9,12 +11,15 @@ import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
 import com.hypixel.hytale.server.core.entity.EntityUtils;
 import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.plugin.PluginManager;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
 import com.azuredoom.levelingcore.api.LevelingCoreApi;
+
+import java.util.logging.Level;
 
 public class XPTickSystem extends EntityTickingSystem<EntityStore> {
 
@@ -36,7 +41,12 @@ public class XPTickSystem extends EntityTickingSystem<EntityStore> {
         }
         LevelingCoreApi.getLevelServiceIfPresent().ifPresent(levelService1 -> {
             var xpHud = new XPBarHud(playerRef, levelService1);
-            MultipleHUD.getInstance().setCustomHud(player, playerRef, "levelingcore_xpbar", xpHud);
+            if (PluginManager.get().getPlugin(new PluginIdentifier("Buuz135", "MultipleHUD")) != null) {
+                MultipleHUD.getInstance().setCustomHud(player, playerRef, "levelingcore_xpbar", xpHud);
+            } else {
+                LevelingCore.LOGGER.at(Level.WARNING).log("MultipleHUD not found, XP HUD will not work correctly with other mods adding custom UI");
+                player.getHudManager().setCustomHud(playerRef, xpHud);
+            }
         });
     }
 
