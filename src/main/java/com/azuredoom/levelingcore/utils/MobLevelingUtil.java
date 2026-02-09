4cfc5f836a439c9ed0cb718f825eaec28de581b8
KabanFriends <yuukun2004@protonmail.ch>
2026-02-10 06:19:07 +0900

    small regression

--- src/main/java/com/azuredoom/levelingcore/utils/MobLevelingUtil.java
+++ src/main/java/com/azuredoom/levelingcore/utils/MobLevelingUtil.java
@@ -71,17 +71,19 @@ public class MobLevelingUtil {
         if (npc.getReference() == null || !npc.getReference().isValid())
             return false;
 
-        var healthMult = 1F + ((float) level - 1F) * config.get().getMobHealthMultiplier();
-        var stats = store.getComponent(npc.getReference(), EntityStatMap.getComponentType());
-        var healthIndex = DefaultEntityStatTypes.getHealth();
-        var modifier = new StaticModifier(
-            Modifier.ModifierTarget.MAX,
-            StaticModifier.CalculationType.ADDITIVE,
-            healthMult
-        );
-        stats.putModifier(healthIndex, "LevelingCore_mob_health", modifier);
-        stats.maximizeStatValue(EntityStatMap.Predictable.SELF, DefaultEntityStatTypes.getHealth());
-        stats.update();
+        store.getExternalData().getWorld().execute(() -> {
+            var healthMult = 1F + ((float) level - 1F) * config.get().getMobHealthMultiplier();
+            var stats = store.getComponent(npc.getReference(), EntityStatMap.getComponentType());
+            var healthIndex = DefaultEntityStatTypes.getHealth();
+            var modifier = new StaticModifier(
+                Modifier.ModifierTarget.MAX,
+                StaticModifier.CalculationType.ADDITIVE,
+                healthMult
+            );
+            stats.putModifier(healthIndex, "LevelingCore_mob_health", modifier);
+            stats.maximizeStatValue(EntityStatMap.Predictable.SELF, DefaultEntityStatTypes.getHealth());
+            stats.update();
+        });
 
         return true;
     }
